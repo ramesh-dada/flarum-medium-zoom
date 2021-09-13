ce20ded79f42e20eae63a97d6494acbac0f002bd
RameshDada <iusewgogoi@gmail.com>
2021-09-13 12:31:36 +0530

    Stable (maybe)

---
+++ README.md
@@ -0,0 +1,27 @@
+# Medium Zoom
+
+![License](https://img.shields.io/badge/license-MIT-blue.svg) [![Latest Stable Version](https://img.shields.io/packagist/v/ramesh-dada/flarum-medium-zoom.svg)](https://packagist.org/packages/ramesh-dada/flarum-medium-zoom) [![Total Downloads](https://img.shields.io/packagist/dt/ramesh-dada/flarum-medium-zoom.svg)](https://packagist.org/packages/ramesh-dada/flarum-medium-zoom)
+
+A [Flarum](http://flarum.org) extension.
+Integrate [medium-zoom](https://github.com/francoischalifour/medium-zoom#readme) into [Flarum](https://flarum.org/), which can provide the ability to zoom images.
+
+## Installation
+
+Install with composer:
+
+```sh
+composer require ramesh-dada/flarum-medium-zoom:"*"
+```
+
+## Updating
+
+```sh
+composer update ramesh-dada/flarum-medium-zoom:"*"
+php flarum migrate
+php flarum cache:clear
+```
+
+## Links
+
+- [Packagist](https://packagist.org/packages/ramesh-dada/flarum-medium-zoom)
+- [GitHub](https://github.com/ramesh-dada/flarum-medium-zoom)
---
+++ extend.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of ramesh-dada/flarum-medium-zoom.
+ *
+ * Copyright (c) 2021 RameshDada.
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+namespace RameshDada\MediumZoom;
+
+use Flarum\Extend;
+use Flarum\Frontend\Document;
+
+return [
+    (new Extend\Frontend('forum'))
+    ->content(function (Document $document) {
+        $document->head[] = '<script src="https://cdnjs.cloudflare.com/ajax/libs/medium-zoom/1.0.6/medium-zoom.min.js"></script>';
+        $document->foot[] = <<<HTML
+<script>
+flarum.core.compat.extend.extend(flarum.core.compat['components/CommentPost'].prototype, 'oncreate', function (output, vnode) {
+const self = this;
+this.$('img').not('.emoji').not(".Avatar").not($(".PostMeta-ip img")).each(function () {
+    try {
+        $().ready(function(){
+            const zoom = mediumZoom();
+            zoom.attach($(this));
+        })
+    } catch (e) {
+        console.error(e.name);
+        console.error(e.message);
+    }
+});
+});
+</script>
+HTML;
+    })
+];
